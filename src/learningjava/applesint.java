8ee5d4098f922ee2b0e7855a6f35c23836c352d4
Maurmair <maurmair@gmail.com>
2017-02-18 11:44:18 +0100

    tst

---
+++ src/learningjava/applesint.java
@@ -0,0 +1,15 @@
+package learningjava;
+import java.util.Scanner;
+
+public class applesint {
+
+	public static void main (String args[])
+	{
+		Scanner wes = new Scanner(System.in);
+		int girls, boys, people;
+		girls = 11;
+		boys = 3;
+		people = girls / boys;
+		System.out.println(people);
+	}
+}
