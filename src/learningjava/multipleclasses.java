6417b7f0249f8d47149370cbc494dc166200b265
Maurmair <maurmair@gmail.com>
2017-02-18 13:02:51 +0100

    Multiple classes

---
+++ src/learningjava/multipleclasses.java
@@ -0,0 +1,9 @@
+package learningjava;
+
+public class multipleclasses {
+
+	public static void main (String args[]){
+		multipleclassesone multipleclassesoneObject = new multipleclassesone();
+		multipleclassesoneObject.simpleMessage();
+	}
+}
---
+++ src/learningjava/multipleclassesone.java
@@ -0,0 +1,12 @@
+package learningjava;
+
+public class multipleclassesone {
+
+	public static void main (String args[]){
+		
+	}
+	
+	public void simpleMessage(){
+		System.out.println("This is another class");
+	}
+}
