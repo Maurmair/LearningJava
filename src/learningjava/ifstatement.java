5ab85772420189f172fa2b2487002b13e33cbe5e
Maurmair <maurmair@gmail.com>
2017-02-18 12:28:14 +0100

    tst

---
+++ src/learningjava/ifstatement.java
@@ -0,0 +1,19 @@
+package learningjava;
+import java.util.*;
+
+public class ifstatement 
+{
+ public static void main (String args[])
+ {
+	 int test = 6;
+	 
+	 if (test == 9)
+	 {
+		 System.out.println("Yes");
+	 }
+	 else
+	 {
+		 System.out.println("Voorwaarde niet gehaald");
+	 }
+ }
+}
---
+++ src/learningjava/incrementing.java
@@ -0,0 +1,16 @@
+package learningjava;
+import java.util.*;
+
+public class incrementing 
+{
+	public static void main (String args[])
+	{
+		//Scanner wes = new Scanner(System.in);
+		int tuna = 5;
+		//int bass = 18;
+		
+		tuna += 28;
+		System.out.println(tuna);
+		
+	}
+}
