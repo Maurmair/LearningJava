5f024719a4b44a03fd7f8a91a2337d4519323f2f
Maurmair <maurmair@gmail.com>
2017-02-20 13:44:25 +0100

    Multi dimensional arrays

---
+++ src/learningjava/multidimensionalarrays.java
@@ -0,0 +1,8 @@
+package learningjava;
+
+public class multidimensionalarrays {
+	public static void main(String args[]){
+		int firstarray[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+		int secondarray[][]={{30,31,32},{34},{35,36}};
+	}
+}
