8e7b2e21930b76afca90703c9e01826664edfa73
Maurmair <maurmair@gmail.com>
2017-02-20 11:33:35 +0100

    Simple averaging program

---
+++ src/learningjava/simpleaveragingprogram.java
@@ -0,0 +1,21 @@
+package learningjava;
+import java.util.Scanner;
+
+public class simpleaveragingprogram {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int total = 0;
+		int grade;
+		int average;
+		int counter = 0;
+		
+		while (counter < 10){
+			System.out.println("Enter mark " + (counter+1));
+			grade = input.nextInt();
+			total += grade;
+			counter++;
+		}
+		average = total / 10;
+		System.out.println("Your average is " + average);
+	}
+}
