618776a0fb76af9c133ee663479d203027962f61
Maurmair <maurmair@gmail.com>
2017-02-20 12:01:00 +0100

    Do while loops

---
+++ src/learningjava/dowhileloops.java
@@ -0,0 +1,11 @@
+package learningjava;
+
+public class dowhileloops {
+	public static void main(String args[]){
+		int counter = 1;
+		do{ // Voer tenminste één keer uit
+			System.out.println(counter);
+			counter++;
+		}while(counter <=10);
+	}
+}
