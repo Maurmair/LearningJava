816c2569552f32d24dab8c81fdc4a3720012bbc6
Maurmair <maurmair@gmail.com>
2017-02-20 12:40:22 +0100

    Introduction to arrays

---
+++ src/learningjava/introtoarrays.java
@@ -0,0 +1,16 @@
+package learningjava;
+
+public class introtoarrays {
+	public static void main(String args[]){
+		int wes[] = new int[10];
+		
+		wes[0] = 87;
+		wes[1] = 543;
+		wes[2] = 11;
+		wes[9] = 1;
+		
+		int wess[] = {2,4,5,6,7,1,4};
+		System.out.println(wes[1]);
+		System.out.println(wess[1]);
+	}
+}
