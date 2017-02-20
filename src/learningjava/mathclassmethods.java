3b3d17191c5e2e525df272c5494de4fb4a7bef71
Maurmair <maurmair@gmail.com>
2017-02-20 12:19:24 +0100

    Math methodes with comments

---
+++ src/learningjava/mathclassmethods.java
@@ -0,0 +1,15 @@
+package learningjava;
+
+public class mathclassmethods {
+	public static void main(String args[]){
+		// Math.abs -> Geeft de absolute waarde van een getal
+		// Math.ceil -> Rondt een bestaand getal af na boven
+		// Math.floor -> Rondt een bestaand getal af na onder
+		// Math.max -> geeft je het grootste getal van een reeks getallen
+		// Math.min -> geeft je het kleinste getal van een reeks getallen
+		// Math.pow -> getal 1 tot de macht 2
+		// Math.sqrt -> Geeft de wortel van een getal
+		System.out.println(Math.min(7.1, 7.5) );
+		
+	}
+}
