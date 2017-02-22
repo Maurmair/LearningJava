e96dc006a3701739f36de5488d739a7807907ba9
Maurmair <maurmair@gmail.com>
2017-02-22 12:34:37 +0100

    Static variables

---
+++ src/learningjava/statischone.java
@@ -0,0 +1,12 @@
+package learningjava;
+
+public class statischone {
+	public static void main (String args[]){
+		
+		statisch mone = new statisch("Wes","Maurmair");
+		statisch mtwo = new statisch("Nora","Miessen");
+		statisch mthree = new statisch("Mitch","Maurmair");
+		
+		
+	}
+}
